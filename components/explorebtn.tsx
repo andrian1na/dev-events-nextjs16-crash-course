4d6ab0ec5c63859601c77651a316e71570d06821
Your Name <ramiandrisoaandrianina01@gmail.com>
2026-09-03 21:00:11 +0400

    Implement PostHog

--- components/explorebtn.tsx
+++ components/explorebtn.tsx
@@ -1,9 +1,18 @@
 'use client';
 import Image from 'next/image';
+import posthog from 'posthog-js';
 
 const Explorebtn = () => {
+    const handleExploreClick = () => {
+      console.log('CLICK');
+
+      if (process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+        posthog.capture('explore_events_clicked');
+      }
+    };
+
     return (
-      <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
+      <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={handleExploreClick}>
       <a href="#events">
           Explore Events
           <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/>
